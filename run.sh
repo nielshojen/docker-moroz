167c36223611cb709b2f88839e052f404b77b08a
Niels Højen <niels@hojen.net>
2019-03-20 16:13:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-/usr/bin/moroz -configs /configs -event-logfile /logs/events -tls-cert /certs/server.crt -tls-key /certs/server.key -http-addr=:8080
+execServe="/usr/bin/moroz"
+
+if [[ ${CONFIGS_DIR} ]]; then
+    execServe="${execServe} -configs ${CONFIGS_DIR}"
+else
+    execServe="${execServe} -configs /configs"
+fi
+
+if [[ ${EVENT_LOGFILE} ]]; then
+    execServe="${execServe} -event-logfile ${EVENT_LOGFILE}"
+else
+    execServe="${execServe} -event-logfile /logs/events"
+fi
+
+if [[ ${TLS_CERT} ]]; then
+    echo ${TLS_CERT} > /certs/server.crt
+    execServe="${execServe} -tls-cert ${TLS_CERT}"
+else
+    if [[ -f "/certs/server.crt" ]]; then
+        execServe="${execServe} -tls-cert /certs/server.crt"
+    fi
+fi
+
+if [[ ${TLS_KEY} ]]; then
+    echo ${TLS_KEY} > /certs/server.key
+    execServe="${execServe} -tls-key ${TLS_KEY}"
+else
+    if [[ -f "/certs/server.crt" ]]; then
+        execServe="${execServe} -tls-key /certs/server.key"
+    fi
+fi
+
+if [[ ${HTTP_ADDR} ]]; then
+    execServe="${execServe} -http-addr=${HTTP_ADDR}"
+else
+    execServe="${execServe} -http-addr=:8080"
+fi
+
+echo "Starting using: $execServe"
+
+eval $execServe
