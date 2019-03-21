88f159ad91fa09c114e27e6ea662f0ee507dcc01
Niels Højen <niels@hojen.net>
2019-03-21 19:11:48 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,19 +4,19 @@ echo ">>> Starting Moroz Server <<<"
 
 execServe="/usr/bin/moroz"
 
-if [[ -n ${CONFIGS_DIR} ]]; then
+if [[ ! -z ${CONFIGS_DIR} ]]; then
     execServe="${execServe} -configs ${CONFIGS_DIR}"
 else
     execServe="${execServe} -configs /configs"
 fi
 
-if [[ -n ${EVENT_LOGFILE} ]]; then
+if [[ ! -z ${EVENT_LOGFILE} ]]; then
     execServe="${execServe} -event-logfile ${EVENT_LOGFILE}"
 else
     execServe="${execServe} -event-logfile /logs/events"
 fi
 
-if [[ -n ${TLS_CERT} ]]; then
+if [[ ! -z ${TLS_CERT} ]]; then
     echo "TLS_CERT Set"
     echo "${TLS_CERT}" > /certs/server.crt
     execServe="${execServe} -tls-cert ${TLS_CERT}"
@@ -26,7 +26,7 @@ else
     fi
 fi
 
-if [[ -n ${TLS_KEY} ]]; then
+if [[ ! -z ${TLS_KEY} ]]; then
     echo "TLS_KEY Set"
     echo "${TLS_KEY}" > /certs/server.key
     execServe="${execServe} -tls-key ${TLS_KEY}"
@@ -36,7 +36,7 @@ else
     fi
 fi
 
-if [[ -n ${HTTP_ADDR} ]]; then
+if [[ ! -z ${HTTP_ADDR} ]]; then
     execServe="${execServe} -http-addr=${HTTP_ADDR}"
 else
     execServe="${execServe} -http-addr=:8080"
